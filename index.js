5f97f6c8a20d735373e0e4b8c4346dd71502828b
Stella Nthenya <stellaalogi@gmail.com>
2023-04-03 00:06:42 +0300

    added 3 functions for functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// this is a function of saturday activity with a default
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(firstParam = "go to the office") {
+    return `This Monday, I will ${firstParam}.`
+};
+
+function wrapAdjective(adjective = "*") {
+    return function(message) {
+        return`You are ${adjective}${message}${adjective}!`
+    };
+
+}
